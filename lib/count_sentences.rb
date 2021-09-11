a89cd828d0fb91ec79287ebf45e1bbe308458449
erin <erinverkoulen93@gmail.com>
2021-09-11 15:48:18 -0500

    commit tree ef42b22beed3b43d4392a637ff12dcc6ba192bcd

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
+ 
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentence_split = self.split(/\.|\?|\!/).reject(&:empty?)
+    sentence_split.count
   end
 end
+
+#monkey patching: the practice of adding methods to or altering Ruby's built-in classes (ie. adding an instance method to Ruby's String class)
+#--dangerous, and should almost never be used in real application
